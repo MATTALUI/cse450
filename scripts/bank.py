d2fbc5ae363d3c3be99dc4d4e85bed2fb71d2d98
Matt Hummer <biggreyson@gmail.com>
2024-02-06 21:11:29 -0700

    ???

--- scripts/bank.py
+++ scripts/bank.py
@@ -55,7 +55,8 @@ bank_data = bank_data.rename(columns=name_map)
 # print(bank_data.head())
 
 # Base Report for getting to know the data
-# base_report = utils.BaseReport(bank_data)
+base_report = utils.BaseReport(bank_data)
+# print(base_report.continuous_features)
 # for col in base_report.categorical_features:
 #     if "unknown" in base_report.dataframe[col].unique():
 #         print(
@@ -67,19 +68,24 @@ bank_data = bank_data.rename(columns=name_map)
 # base_report.dataframe.replace(to_replace="services", value=None, inplace=True)
 # base_report.display()
 
+small_bank = bank_data.head(10)
+print(small_bank.head(10))
+# print(small_bank[Cols.marital].unique().tolist().index())
+small_bank[Cols.marital] = small_bank[Cols.marital].map(lambda v: small_bank[Cols.marital].unique().tolist().index(v))
+print(small_bank.head(10))
 
+# features_we_care_about = [Cols.default, Cols.housing, Cols.loan]
+# targets = [Cols.subscribed]
 
-features_we_care_about = [Cols.default, Cols.housing, Cols.loan]
-targets = [Cols.subscribed]
+# x = pd.get_dummies(bank_data[features_we_care_about], dtype=int)
+# y = bank_data[targets].map(lambda t: { "yes": 1, "no": 0}[t])
+# x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=.2)
 
-x = pd.get_dummies(bank_data[features_we_care_about], dtype=int)
-y = bank_data[targets].map(lambda t: { "yes": 1, "no": 0}[t])
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=.2)
+# classifier = tree.DecisionTreeClassifier()
+# classifier = classifier.fit(x_train, y_train)
 
-classifier = tree.DecisionTreeClassifier()
-classifier = classifier.fit(x_train, y_train)
+# predictions = classifier.predict(x_test)
+# score = accuracy_score(y_test, predictions)
+# print(predictions)
+# print(score)
 
-predictions = classifier.predict(x_test)
-score = accuracy_score(y_test, predictions)
-print(predictions)
-print(score)
